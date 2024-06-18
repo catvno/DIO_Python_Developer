7b4e30f0ae02026d1046ba876e34c6e104c75303
Catarina Oliveira <121497517+catvno@users.noreply.github.com>
2024-06-18 11:44:48 -0300

    Update sistema_bancario.py
    Ajustes na estética do código.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -13,34 +13,46 @@ LIMITE_SAQUES = 3
 
 while True:
   opcao = input(menu)
-
+  
+  #################### DEPOSITAR ####################
   if opcao == 'd':
     valor = float(input('Informe o valor que deseja depositar: '))
+    
     if valor > 0:
       saldo += valor
       extrato += f'Depósito: R$ {valor:.2f}\n'
       print('Depósito realizado com sucesso.')
+    
     else:
       print('Valor inválido')
   
+  ###################### SACAR ######################
   elif opcao == 's':
     valor = float(input('Informe o valor que deseja sacar: '))
+    
     if numero_saques <= LIMITE_SAQUES:
+      
       if valor <= saldo and valor <= limite:
         saldo -= valor
         extrato += f'Saque: R$ {valor:.2f}\n'
         numero_saques += 1
         print('Saque realizado com sucesso.')
+      
       elif valor > limite:
         print(f'O valor informado excede o limite de R${limite}.00 por saque.')
+      
       else:
         print('Saldo insuficiente.')
+    
     else: 
       print(f'Você ja atingiu o limite de saques diário correspondente a {LIMITE_SAQUES}.')
   
+  ###################### EXTRATO ######################
   elif opcao == 'e':
+    
     if extrato == "":
       print('Não foram realizadas movimentações.')
+    
     else:
       print('\n================ EXTRATO ================')
       print(f'\nSaldo Atual: R$ {saldo:.2f}')
@@ -49,8 +61,10 @@ while True:
       print(extrato)
       print('==========================================')
   
+  ###################### SAIR ######################
   elif opcao == 'q':
     break
   
+  ###################### OUTROS ####################
   else:
     print('Operação inválida.')
